840ccf399e75695ebc7cd1927dc3af426c682f0b
Luc Kury <luc.kury@gmail.com>
2020-08-21 15:33:27 +0200

    [CHANGE] Adds session handling with handshake to FetchTool

--- PiCN/ProgramLibs/Fetch/FetchSessions.py
+++ PiCN/ProgramLibs/Fetch/FetchSessions.py
@@ -1,4 +1,4 @@
-"""Fetch Tool for PiCN"""
+"""Fetch Tool for PiCN supporting sessions"""
 
 from PiCN.Packets import Packet
 from PiCN.ProgramLibs.Fetch import Fetch
@@ -9,22 +9,22 @@ from typing import Optional, Dict
 
 
 class FetchSessions(Fetch):
-    """Fetch Tool for PiCN"""
+    """Fetch Tool for PiCN supporting sessions"""
 
     def __init__(self, ip: str, port: int, log_level=255, encoder: BasicEncoder = None, autoconfig: bool = False,
                  interfaces=None, session_keys: Optional[Dict] = None):
         super().__init__(ip, port, log_level, encoder, autoconfig, interfaces)
 
-        self.session_keys: Dict = dict() if session_keys is None else session_keys  # TODO: Extend this to work with multiple repos (use dict or something).
-        self.has_session: bool = True if session_keys is not None else False
-        self.session_identifier = 'session_connector'
+        self._session_keys: Dict = dict() if session_keys is None else session_keys  # TODO: Extend this to work with multiple repos (use dict or something).
+        self._has_session: bool = True if session_keys is not None else False
+        self._session_identifier = 'session_connector'
 
-    def fetch_data_session(self, name: Name, timeout=4.0) -> Optional[str]:
+    def fetch_data_session(self, name: Name, timeout=4.0) -> Optional[str]:  # TODO: Combine method with other fetch method and use bool to ignore session key.
         """Fetch data from the server via a session
         :param name Name to be fetched
         :param timeout Timeout to wait for a response. Use 0 for infinity
         """
-        if not self.has_session:
+        if not self._has_session:
             return "Initialize session with repository first. Send interest to session_connector content object."
         else:
             interest: Interest = Interest(name)  # create interest
@@ -46,20 +46,36 @@ class FetchSessions(Fetch):
 
             return None
 
-    def handle_new_session(self, packet: Packet):
+    def handle_new_session(self, name: Name, packet: Packet) -> None:
         """
+        :param name Name to be fetched
         :param packet Packet with session handshake
         """
         if isinstance(packet, Content):
-            self.session_keys[packet.name] = packet.content
-            self.has_session = True
+            session_confirmation: Interest = Interest(name + Name(f"{self._session_identifier}/{packet.content}"))
+
+            # Send session ACK
+            if self.autoconfig:
+                self.lstack.queue_from_higher.put([None, session_confirmation])
+            else:
+                self.lstack.queue_from_higher.put([self.fid, session_confirmation])
+
+            self._session_keys[packet.name] = packet.content
+            self._has_session = True
+
+    def end_session(self, name: Name) -> None:
+        """
+        param name Name to terminate session with
+        """
+        # TODO: Implement method. Send interest in the form of Name(/test/t2/session/<id>/remove). Wait for ACK.
+        pass
 
     def fetch_data(self, name: Name, timeout=4.0) -> Optional[str]:
         """Fetch data from the server
         :param name Name to be fetched
         :param timeout Timeout to wait for a response. Use 0 for infinity
         """
-        interest: Interest = Interest(name)  # create interest
+        interest: Interest = Interest(name)  # Create interest
 
         if self.autoconfig:
             self.lstack.queue_from_higher.put([None, interest])
@@ -72,12 +88,11 @@ class FetchSessions(Fetch):
             packet = self.lstack.queue_to_higher.get(timeout=timeout)[1]
 
         if isinstance(packet, Content):
-            if self.session_identifier in packet.name.components_to_string():
-                self.handle_new_session(packet)
-                return packet.content
-            else:
-                return packet.content
-        if isinstance(packet, Nack):
+            if self._session_identifier in packet.name.components_to_string():  # Check for second message of handshake
+                self.handle_new_session(name, packet)
+
+            return packet.content
+        elif isinstance(packet, Nack):
             return "Received Nack: " + str(packet.reason.value)
 
         return None
