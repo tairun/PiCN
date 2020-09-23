3a989fd1e379834fd49c344a8da8b9be748b28a5
Luc Kury <luc.kury@gmail.com>
2020-09-23 20:22:09 +0200

    [CHANGE] Handling sessions and storing them in RepoLayer

--- PiCN/Layers/RepositoryLayer/SessionRepositoryLayer.py
+++ PiCN/Layers/RepositoryLayer/SessionRepositoryLayer.py
@@ -16,11 +16,12 @@ class SessionRepositoryLayer(LayerProcess):
     def __init__(self, repository: BaseRepository, propagate_interest: bool = False, logger_name="RepoLayer", log_level=255):
         super().__init__(logger_name, log_level)
 
-        self._session_identifier: str = 'session_connector'
-        self._pending_sessions: List = []  # TODO: Implement session initiation procedure (handshake).
-        self._running_sessions: List = []  # TODO: Implement better data structure to handle sessions. HashMap?
         self._repository: BaseRepository = repository
         self._propagate_interest: bool = propagate_interest
+        self._session_initiator: str = 'session_connector'
+        self._session_identifier = 'sid'
+        self._pending_sessions: List[str] = []  # TODO: Implement session initiation procedure (handshake).
+        self._running_sessions: List[str] = []  # TODO: Implement better data structure to handle sessions. HashMap?
 
     def _make_session_id(self, bits: int = 16) -> str:
         """
@@ -41,28 +42,16 @@ class SessionRepositoryLayer(LayerProcess):
         faceid: int = data[0]
         packet: Packet = data[1]
 
-        self.logger.info(f"--> Last component of name: {packet.name.components[-1].decode()}")  # FIXME: Delete when done.
-
-        if isinstance(packet, Interest):  # TODO: Can I check here for connector interest and return session key? Yes!!
-            # self.logger.info(packet)  # FIXME: Delete when done.
-            if self._session_identifier in packet.name.components_to_string():
+        if isinstance(packet, Interest):
+            if self._session_initiator in packet.name.components_to_string():
                 self.logger.info('--> : Session packet detected.')
-                # self.logger.info(type(packet.name.components[-1].decode()))  # FIXME: Delete when done.
-                if packet.name.components[-1].decode() == self._session_identifier:  # Detect incoming handshake (if session_connector is the last component of interest name)
+                if packet.name.components[-1].decode() == self._session_initiator:  # Detect incoming handshake (if session_connector is the last component of interest name)
                     session_id: str = self._make_session_id()
                     self._pending_sessions.append(session_id)
                     c = Content(packet.name, session_id, None)
                     self.logger.info('--> : Request to initiate session. Sending key down.')
                     self.queue_to_lower.put([faceid, c])  # Sending session id as content packet
                     return
-                elif packet.name.components[-1].decode() in self._pending_sessions:  # Detect third part of handshake and store session (if session id is last part of interest name)
-                    session_id: str = packet.name.components[-1].decode()
-                    self._pending_sessions.remove(session_id)
-                    self._running_sessions.append(session_id)
-                    self.logger.info(f"--> : Session with id {session_id} established.")
-                    c = Content(packet.name, None, None)
-                    self.logger.info('--> : Sending empty content to stop retransmit.')  # FIXME: Better solution to not trigger retransmits?
-                    self.queue_to_lower.put([faceid, c])  # Sending session id as content packet
                 else:
                     self.logger.info('--> : Unknown session packet receveid. Dropping and sending nack.')
                     nack = Nack(packet.name, NackReason.NO_CONTENT, interest=packet)
@@ -82,7 +71,16 @@ class SessionRepositoryLayer(LayerProcess):
                 to_lower.put([faceid, nack])  # FIXME: queue_to_lower or just to lower? Other instances above?
                 return
         elif isinstance(packet, Content):  # FIXME: Better use elif here? We need to handle incoming content for sessions!
-            self.logger.info(f"---> : Got content WTF ({packet.content})")
+            self.logger.info(f"--> : Got content WTF ({packet.content})")
+            if self._session_identifier in packet.name.to_string() and packet.name.components[-1].decode() in self._pending_sessions:  # Detect third part of handshake and store session (if session id is last part of interest name)
+                session_id: str = packet.name.components[-1].decode()
+                self._running_sessions.append(session_id)
+                self._pending_sessions.remove(session_id)
+                self.logger.info(f"--> : Session with id {session_id} established.")
+                self.logger.info(f"--> : Running sessions for repo: {self._running_sessions}")
+                #c = Content(packet.name, None, None)
+                #self.logger.info('--> : Sending empty content to stop retransmit.')  # FIXME: Better solution to not trigger retransmits?
+                #self.queue_to_lower.put([faceid, c])  # Sending session id as content packet
             return
         else:
             self.logger('No known packet type.')
